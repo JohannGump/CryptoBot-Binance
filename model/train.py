04a538119f3df668deca7d62120ba4ae71f7e172
Bonbek <nantistress@gmail.com>
2023-07-30 20:26:39 +0200

    Make full model reloadable.
    Include preprocessing layers and trying to "adhere" Tensorflow specs in order
    to minimize clashes when deploying with Tensorflow Serving.

--- model/train.py
+++ model/train.py
@@ -1,63 +1,68 @@
-#%%
+"""Model training and building.
+
+Core keras RNN wrapped into final model with integrated input (pre)processing.
+Models are saved into /model_fit/<timestep unit>/<version>, ex:
+    /model_fit/hourly/1
+"""
+
 import sys
 import os
+
 sys.path.insert(0, os.path.abspath("../"))
 sys.path.insert(0, os.path.abspath("../binance_bridge"))
 from binance_bridge.schemas import Symbol
-from preprocessing import vwap
-from model import Model
+import preprocessing as pp
 
-import numpy as np
 import pandas as pd
-from sklearn.preprocessing import StandardScaler
 import tensorflow as tf
-import joblib
 
-#%% Load historical data
-test_s = 4
-window = 4
-seqs_s = 4
+# Core model training
+# -------------------
 
-datas= {}
+INPUT_SEQUENCE_LENGTH = 4
+OUTPUT_TIMESTEP = 4
+TEST_SAMPLES_N = INPUT_SEQUENCE_LENGTH * 3
+
+feats = None
+targs = None
 for symbol in Symbol:
-    datas[symbol] = pd.read_csv(f'../training_data/{symbol.value}.csv')[:-test_s]
+    asset = pd.read_csv(f'../training_data/{symbol.value}.csv')[:-TEST_SAMPLES_N] \
+        .set_index('Open Time').sort_index()[pp.RAW_FEATURES]
+    if feats is None:
+        feats = pd.DataFrame(index=asset.index)
+        targs = pd.DataFrame(index=asset.index)
+    feats = feats.join(pp.compute_features(asset), lsuffix=symbol.value)
+    targs = targs.join(pp.compute_targets(asset, OUTPUT_TIMESTEP), lsuffix=symbol.value)
 
-cryptos = {}
-columns = ['Open Price', 'High Price', 'Low Price', 'Close Price', 'Volume']
-for symbol in Symbol:
-    asset = datas.get(symbol).set_index('Open Time')[columns]
-    asset['VWAP'] = vwap(asset)
-    asset['Upper Shadow'] = asset['High Price'] - np.maximum(asset['Close Price'], asset['Open Price'])
-    asset['Lower Shadow'] = np.minimum(asset['Close Price'], asset['Open Price']) - asset['Low Price']
-    asset[f'Returns +{window}'] = asset['Close Price'].pct_change(periods=window).shift(-window)
-    cryptos[symbol] = asset[:-window]
+feats = feats[:-INPUT_SEQUENCE_LENGTH]
+targs = targs[:-INPUT_SEQUENCE_LENGTH]
 
-index = cryptos[Symbol.ADAUSDT].index
-feats = pd.DataFrame(index=index)
-targs = pd.DataFrame(index=index)
-for symbol, asset in cryptos.items():
-    feats[f'VWAP {symbol.value}'] = asset['VWAP']
-    feats[f'Upper {symbol.value}'] = asset['Upper Shadow']
-    feats[f'Lower {symbol.value}'] = asset['Lower Shadow']
-    targs[f'Returns +{window} {symbol.value}'] = asset[f'Returns +{window}']
+normalizer = tf.keras.layers.Normalization()
+normalizer.adapt(feats.to_numpy().reshape(-1, INPUT_SEQUENCE_LENGTH, feats.shape[1]))
 
-df_Xs = feats
-df_ys = targs
-
-scaler = StandardScaler()
-Xs = scaler.fit_transform(df_Xs)
-
-Xs = tf.keras.utils.timeseries_dataset_from_array(data=Xs, targets=df_ys, sequence_stride=1, sequence_length=seqs_s, batch_size=32)
+Ds = tf.keras.utils.timeseries_dataset_from_array(
+    data=feats, targets=targs, sequence_stride=1,
+    sequence_length=INPUT_SEQUENCE_LENGTH, batch_size=32)
 
 model = tf.keras.models.Sequential()
+model.add(normalizer)
 model.add(tf.keras.layers.LSTM(128, return_sequences=False))
 model.add(tf.keras.layers.Dense(len(targs.columns)))
 
 model.compile(tf.keras.optimizers.Adam(), loss='mse', metrics=['mae'])
-history = model.fit(Xs, epochs=1)
+history = model.fit(Ds, epochs=1)
 
 # TODO: writes log fit history
 
-#%% Save model and scaler
-model_api = Model(model, scaler)
-joblib.dump(model_api, '../model_fit/model_v.joblib')
+# Final model build
+# -----------------
+# Saved in SavedModel's format to support processing layers
+# (https://github.com/keras-team/keras/issues/15348#issuecomment-974747528)
+
+saved_model = tf.keras.models.Sequential()
+saved_model.add(tf.keras.Input(shape=(INPUT_SEQUENCE_LENGTH, len(Symbol))))
+saved_model.add(pp.RawSeqToFeatures(sequence_length=INPUT_SEQUENCE_LENGTH))
+saved_model.add(model)
+
+# TODO: handle parametrizable timestep unit and version
+saved_model.save('../model_fit/hourly/1')
