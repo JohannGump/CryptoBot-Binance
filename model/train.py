179a749e784283992a1517de9b6272ee1ccc19bf
Bonbek <nantistress@gmail.com>
2023-08-02 18:41:37 +0200

    Feat. model build from timestep
    Open door to one model per timestep.
    No fine tune is made depending on timestep, all model as trained with the
    same parameters.

--- model/train.py
+++ model/train.py
@@ -1,10 +1,24 @@
 """Model training and building.
 
 Core keras RNN wrapped into final model with integrated input (pre)processing.
-Models are saved into /model_fit/<timestep unit>/<version>, ex:
-    /model_fit/hourly/1
-"""
+Models are saved into ../model_fit/<timestep>.
 
+Usage
+-----
+Beware that data must be availaible for the given timestep (default to hourly)
+in parent `../training_data/<timestep>` folder.
+
+Build an hourly timestep model (execute cmd from the script's folder)
+>>> python train.py
+
+Build a model for another timestep, define the MODEL_TIMESTEP env variable
+to the desired timsetep
+>>> MODEL_TIMESTEP=weekly python train.py
+
+Should give us to saved models
+    ../model_fit/hourly
+    ../model_fit/weekly
+"""
 import sys
 import os
 
@@ -19,6 +33,7 @@ import tensorflow as tf
 # Core model training
 # -------------------
 
+TIMESTEP = os.getenv('MODEL_TIMESTEP', 'hourly')
 INPUT_SEQUENCE_LENGTH = 4
 OUTPUT_TIMESTEP = 4
 TEST_SAMPLES_N = INPUT_SEQUENCE_LENGTH * 3
@@ -26,19 +41,20 @@ TEST_SAMPLES_N = INPUT_SEQUENCE_LENGTH * 3
 feats = None
 targs = None
 for symbol in Symbol:
-    asset = pd.read_csv(f'../training_data/{symbol.value}.csv')[:-TEST_SAMPLES_N] \
+    asset = pd.read_csv(f'../training_data/{TIMESTEP}/{symbol.value}.csv')[:-TEST_SAMPLES_N] \
         .set_index('Open Time').sort_index()[pp.RAW_FEATURES]
     if feats is None:
         feats = pd.DataFrame(index=asset.index)
         targs = pd.DataFrame(index=asset.index)
-    feats = feats.join(pp.compute_features(asset), lsuffix=symbol.value)
-    targs = targs.join(pp.compute_targets(asset, OUTPUT_TIMESTEP), lsuffix=symbol.value)
+    feats = feats.join(pp.compute_features(asset), rsuffix=f' {symbol.value}')
+    targs = targs.join(pp.compute_targets(asset, OUTPUT_TIMESTEP), rsuffix=f' {symbol.value}')
 
 feats = feats[:-INPUT_SEQUENCE_LENGTH]
 targs = targs[:-INPUT_SEQUENCE_LENGTH]
 
 normalizer = tf.keras.layers.Normalization()
-normalizer.adapt(feats.to_numpy().reshape(-1, INPUT_SEQUENCE_LENGTH, feats.shape[1]))
+rounded = (len(feats)  // INPUT_SEQUENCE_LENGTH) * INPUT_SEQUENCE_LENGTH
+normalizer.adapt(feats[:rounded].to_numpy().reshape(-1, INPUT_SEQUENCE_LENGTH, feats.shape[1]))
 
 Ds = tf.keras.utils.timeseries_dataset_from_array(
     data=feats, targets=targs, sequence_stride=1,
@@ -64,5 +80,5 @@ saved_model.add(tf.keras.Input(shape=(INPUT_SEQUENCE_LENGTH, len(Symbol))))
 saved_model.add(pp.RawSeqToFeatures(sequence_length=INPUT_SEQUENCE_LENGTH))
 saved_model.add(model)
 
-# TODO: handle parametrizable timestep unit and version
-saved_model.save('../model_fit/hourly/1')
+# TODO: handle model version
+saved_model.save(f'../model_fit/{TIMESTEP}/1')
