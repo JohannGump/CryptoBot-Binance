3e7b9b4addc521b1b2ef15d316a3d2e5b0adebf0
jlebot44 <lebot.julien@gmail.com>
2023-08-22 11:43:28 +0200

    add import binance

--- data/build_history_test.py
+++ data/build_history_test.py
@@ -1,2 +1,7 @@
 from build_history import connect
-import mysql.connector
+import sys
+import os
+sys.path.insert(0, os.path.abspath("../"))
+sys.path.insert(0, os.path.abspath("../binance_bridge"))
+from binance_bridge.schemas import Symbol, TimeStep
+from binance_bridge.klines import binance_raw_klines, raw_klines_to_pandas
