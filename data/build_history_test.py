b70743af44708aad4efc496783e4859ef3cdb967
jlebot44 <lebot.julien@gmail.com>
2023-08-22 11:58:04 +0200

    test build_history_test

--- data/build_history_test.py
+++ data/build_history_test.py
@@ -1,6 +1,6 @@
-# import sys
-# import os
-# sys.path.insert(0, os.path.abspath("../"))
-# sys.path.insert(0, os.path.abspath("../binance_bridge"))
-# from binance_bridge.schemas import Symbol, TimeStep
-# from binance_bridge.klines import binance_raw_klines, raw_klines_to_pandas
+import sys
+import os
+sys.path.insert(0, os.path.abspath("../"))
+sys.path.insert(0, os.path.abspath("../binance_bridge"))
+from binance_bridge.schemas import Symbol, TimeStep
+from binance_bridge.klines import binance_raw_klines, raw_klines_to_pandas
