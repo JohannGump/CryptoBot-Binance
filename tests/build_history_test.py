e16196ace5805411abbe5f30ca95c8b922617e68
jlebot44 <lebot.julien@gmail.com>
2023-08-23 11:41:06 +0200

    create tests folder

--- tests/build_history_test.py
+++ tests/build_history_test.py
@@ -1,10 +1,11 @@
 import sys
 import os
 sys.path.insert(0, os.path.abspath("./"))
+sys.path.insert(0, os.path.abspath("./data"))
 sys.path.insert(0, os.path.abspath("./binance_bridge"))
 from binance_bridge.schemas import Symbol, TimeStep
 from binance_bridge.klines import binance_raw_klines, raw_klines_to_pandas
-from build_history import connect
+from data.build_history import connect
 
 def test_connect():    
     assert True
