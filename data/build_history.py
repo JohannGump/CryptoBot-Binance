da361ab17219747a8fdfa14dc8f5d4689adb1532
Johann <johannforever@gmail.com>
2023-09-13 20:35:01 +0200

    kubernetes CICD and cleaning #4

--- data/build_history.py
+++ data/build_history.py
@@ -19,9 +19,6 @@ from datetime import datetime
 import asyncio
 from tqdm.asyncio import tqdm_asyncio
 import pandas as pd
-sys.path.insert(0, os.path.abspath("./"))
-sys.path.insert(0, os.path.abspath("./binance_bridge"))
-print(sys.path.insert(0, os.path.abspath("./binance_bridge")))
 from binance_bridge.schemas import Symbol, TimeStep
 from binance_bridge.klines import binance_raw_klines, raw_klines_to_pandas
 
