5dd0842d318c60c8e81ad5448499049b604e6074
Bonbek <nantistress@gmail.com>
2023-09-17 17:07:06 +0200

    web-api: home and forecast page done.

--- web_api/main.py
+++ web_api/main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI, Request, Form
+import os
+from fastapi import FastAPI, Request, Form, Depends
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 import mysql.connector
@@ -6,69 +7,152 @@ import uvicorn
 import plotly.graph_objs as go
 from fastapi.responses import JSONResponse
 from fastapi.staticfiles import StaticFiles
-import os
+from datetime import datetime, timedelta
+from enum import Enum
+from typing import Dict, Any
 
-app = FastAPI()
+class Symbol(str, Enum):
+    ADAUSDT = "ADA"
+    BTCUSDT = "BTC"
+    BNBUSDT = "BNB"
+    ETHUSDT = "ETH"
+    XRPUSDT = "XRP"
 
+class SymbolName(str, Enum):
+    ADAUSDT = "Cardano"
+    BTCUSDT = "Bitcoin"
+    BNBUSDT = "BNB"
+    ETHUSDT = "Ethereum"
+    XRPUSDT = "Ripple"
+
+class Frequency(str, Enum):
+    MINUTELY = "minutely"
+    HOURLY = "hourly"
+    DAILY = "daily"
+    WEEKLY = "weekly"
+
+app = FastAPI() 
 app.mount("/static", StaticFiles(directory="static"), name="static")
-templates = Jinja2Templates(directory="templates")
 
-#connect to db
-db = mysql.connector.connect(
-    host=os.getenv('MYSQL_HOST_PREDICTIONS'),
-    user=os.getenv('MYSQL_USER_PREDICTIONS'),
-    password=os.getenv('MYSQL_PASSWORD_PREDICTIONS'),
-    database=os.getenv('MYSQL_DATABASE_PREDICTIONS')
-)
+def template_share_vars(request: Request) -> Dict[str, Any]:
+    return {'Symbol': Symbol, 'SymbolName': SymbolName, 'time': datetime.now() }
 
-@app.get("/", response_class=HTMLResponse)
-def get_home(request: Request):
-    cursor = db.cursor()
-    # Fix temporaire (ref 8bd3bcb)
-    # (re) Formatage resultat (results) du select pour correspondre au code actuel
-    cursor.execute("SELECT * FROM predictions ORDER BY OpenTime, Symbol")
-    raw_results = cursor.fetchall()
-    cursor.close()
-    results = []
-    for i in range(len(raw_results) // 5):
-        records = raw_results[i*5:i*5 + 5]
-        results.append([r[4] for r in records] + [records[0][3]])
+templates = Jinja2Templates(directory="templates", context_processors=[template_share_vars])
+TemplateResponse = templates.TemplateResponse
 
-    ada_values = [result[0] for result in results]
-    bnb_values = [result[1] for result in results]
-    btc_values = [result[2] for result in results]
-    eth_values = [result[3] for result in results]
-    xrp_values = [result[4] for result in results]
-    dates = [result[5] for result in results]
-
-    # Créez les traces pour chaque paire de dates et de valeurs
-    trace_ada = go.Scatter(x=dates, y=ada_values, name='ADAUSDT')
-    trace_bnb = go.Scatter(x=dates, y=bnb_values, name='BNBUSDT')
-    trace_btc = go.Scatter(x=dates, y=btc_values, name='BTCUSDT')
-    trace_eth = go.Scatter(x=dates, y=eth_values, name='ETHUSDT')
-    trace_xrp = go.Scatter(x=dates, y=xrp_values, name='XRPUSDT')
-
-    data = [trace_ada, trace_bnb, trace_btc, trace_eth, trace_xrp]
-
-    layout = go.Layout(
-        title='Évolution des prédictions',
-        xaxis=dict(title='Date', tickformat='%Y-%m-%d %H:%M:%S'),
-        yaxis=dict(title='Valeur de prédiction')
+def connect():
+    conn = mysql.connector.connect(
+        host=os.getenv('MYSQL_HOST_PREDICTIONS'),
+        user=os.getenv('MYSQL_USER_PREDICTIONS'),
+        password=os.getenv('MYSQL_PASSWORD_PREDICTIONS'),
+        database=os.getenv('MYSQL_DATABASE_PREDICTIONS')
     )
+    return conn
 
-    fig = go.Figure(data=data, layout=layout)
+def tmpl_context(request: Request) -> Dict[str, Any]:
+    """Jinja template context mixin"""
+    return lambda **kwargs: {**kwargs, 'request': request}
 
-    graph_html = fig.to_html(full_html=False, default_height=500, default_width=700)
+TemplateVars = Depends(tmpl_context)
 
-    return templates.TemplateResponse("index.html", {"request": request, "graph_html": graph_html})
-
-@app.post("/predictions", response_class=JSONResponse)
-async def get_predictions(request: Request, latest_prediction: list = Form([])):
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM predictions ORDER BY OpenTime DESC LIMIT 1")
-    latest_prediction = cursor.fetchone()
+@app.get('/')
+def index(context = TemplateVars) -> HTMLResponse:
+    """Home page
+    Display the list of 1 hour ahead predictions for all symbols.
+    """
+    now = datetime.now()
+    if now.hour < 2:
+        now = now - timedelta(days=1)
+    now_4days = (now + timedelta(days=4))
+    dbconn = connect()
+    cursor = dbconn.cursor(dictionary=True)
+    query = """
+    SELECT KL.ClosePrice NowClosePrice, PR.*
+      FROM klines KL
+      LEFT JOIN predictions PR
+        ON KL.Symbol = PR.Symbol AND KL.TimeStep = PR.TimeStep
+     WHERE PR.TimeStep = 'DAILY'
+       AND DATE(PR.OpenTime) = %s
+       AND DATE(KL.OpenTime) = %s
+     ORDER BY Symbol
+    """
+    cursor.execute(query, [now_4days.strftime('%Y-%m-%d'), now.strftime('%Y-%m-%d')])
+    datas=cursor.fetchall()
     cursor.close()
-    return {"latest_prediction": latest_prediction}
+    dbconn.close()
+
+    tmpl_vars = context(
+        pred_date=now_4days,
+        predictions=datas)
+
+    return TemplateResponse('index.html', tmpl_vars)
+
+@app.get('/forecast/{symbol}/{timestep}')
+def forecast(symbol: Symbol, timestep: Frequency, context = TemplateVars) -> HTMLResponse:
+    dbconn = connect()
+    cursor = dbconn.cursor(dictionary=True)
+
+    # Select most recent klines
+    unit = dict(zip(Frequency, ['MINUTE', 'HOUR', 'DAY', 'WEEK']))[timestep]
+    query = f"""
+    SELECT * FROM klines
+     WHERE Symbol = %s AND TimeStep = %s
+       AND OpenTime >= DATE_SUB(
+           (SELECT MAX(OpenTime) FROM klines
+             WHERE Symbol = %s AND TimeStep = %s),
+           INTERVAL 3 {unit})
+     ORDER BY OpenTime
+    """
+    cursor.execute(query, [symbol.name, timestep.name, symbol.name, timestep.name])
+    klines = cursor.fetchall()
+    last_k = klines[-1].get('OpenTime')
+
+    query = """
+    SELECT * FROM predictions
+     WHERE Symbol = %s AND TimeStep = %s
+       AND OpenTime > %s
+    """
+    cursor.execute(query, [symbol.name, timestep.name, last_k])
+    predictions = cursor.fetchall()
+
+    cursor.close()
+    dbconn.close()
+
+    dates = [x['OpenTime'] for x in klines] + [x['OpenTime'] for x in predictions]
+    ldata = [x['ClosePrice'] for x in klines]
+    rdata = [None]*(len(klines) - 1) + [klines[-1]['ClosePrice']]
+    rdata = rdata + [x['ClosePrice'] for x in predictions]
+
+    fig = go.Figure(
+        data = [
+            go.Line(x=dates, y=ldata, line_color='orange', mode='lines+markers', name=''),
+            go.Line(x=dates, y=rdata, line_dash="dot", line_color='orange', mode='lines+markers', name=''),
+        ],
+        layout = go.Layout(
+            paper_bgcolor='rgba(0,0,0,0)',
+            plot_bgcolor='rgba(0,0,0,0)',
+            margin={ 't': 0, 'r': 0, 'b': 0, 'l': 0 },
+            showlegend=False,
+            yaxis_title=None,
+            xaxis_title=None
+        )
+    )
+    fig.update_yaxes(gridcolor='rgba(0,0,0,.1)')
+    fig.update_xaxes(showgrid=False)
+
+    graph_html = fig.to_html(full_html=True, config = {'displayModeBar': False})
+
+    tmpl_vars = context(
+        now=datetime.now(),
+        unit=dict(zip(Frequency, ['M', 'H', 'J', 'S']))[timestep],
+        unit_word=dict(zip(Frequency, ['Minute', 'Heure', 'Jour', 'Semaine']))[timestep],
+        timestep=timestep.value,
+        klines=klines,
+        predictions=predictions,
+        symbol=symbol.name,
+        graph_html=graph_html
+    )
+    return templates.TemplateResponse("forecast.html", tmpl_vars)
 
 # Lancer l'application FastAPI
 if __name__ == "__main__":
