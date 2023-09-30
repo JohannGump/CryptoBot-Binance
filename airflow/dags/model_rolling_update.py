63e6dfbbd12d6710595a18a1f74e6e4b992258ca
Bonbek <nantistress@gmail.com>
2023-09-30 20:47:45 +0200

    (airflow): add model rolling update dag

---
+++ airflow/dags/model_rolling_update.py
@@ -0,0 +1,36 @@
+import os
+from airflow.decorators import dag, task
+from datetime import datetime, timedelta
+import requests
+
+@dag(start_date=datetime(2023, 9, 30),
+    schedule_interval='@once',
+    catchup=False)
+def models_rolling_update():
+    """
+    ### Trigger Github models fit & deployment workflow
+    """
+    @task
+    def trigger_ghci():
+        """
+        #### Request the Github workflow
+        """
+        url = "https://api.github.com/repos/JohannGump/CryptoBot-Binance/dispatches"
+        gh_token = os.getenv('GITHUB_DISPATCH_TOKEN', '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')
+        headers = {
+            "Accept": "application/vnd.github+json",
+            "Authorization": f"token {gh_token}"
+        }
+        payload = {
+            "event_type": "model-rolling-update",
+            "client_payload": {
+                "max_samples_fit": 20000
+            }
+        }
+        res = requests.post(url, headers=headers, json=payload)
+        if res.status_code > 300:
+            raise Exception(res.json())
+
+    trigger_ghci()
+
+models_rolling_update()
